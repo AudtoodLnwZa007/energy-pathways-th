9a8ec14edc14bd27e9b024cd53a4c3eae415c474
AudtoodLnwZa007 <topsukcharoen@gmail.com>
2026-04-22 04:15:14 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -13,5 +13,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/ชื่อ-repo-ของคุณ/',  // ← เพิ่มบรรทัดนี้
+  base: '/energy-pathways-th/',  // ← เพิ่มบรรทัดนี้
 })
